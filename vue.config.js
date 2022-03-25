4840389291cfdd2dccfec1e6a5d6c1aeeef5fb49
Grayson <gclark1013@gmail.com>
2022-03-25 12:19:44 -0400

    cant imagine

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 // vue.config.js file to be placed in the root of your repository
 
 module.exports = {
-    publicPath: '/FaultyPine.github.io/'
-}
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/FaultyPine.github.io'
+    : '/'
+  }
